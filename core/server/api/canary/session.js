aaaf5548881c08b5b0f28bb65f045e7643f91991
sapphi-red <green@sapphi.red>
2019-12-17 22:01:50 +0900

    自動サインイン(SSO)

--- core/server/api/canary/session.js
+++ core/server/api/canary/session.js
@@ -16,18 +16,7 @@ const session = {
         return models.User.findOne({id: frame.options.context.user});
     },
     add(frame) {
-        const object = frame.data;
-
-        if (!object || !object.username || !object.password) {
-            return Promise.reject(new errors.UnauthorizedError({
-                message: i18n.t('errors.middleware.auth.accessDenied')
-            }));
-        }
-
-        return models.User.check({
-            email: object.username,
-            password: object.password
-        }).then((user) => {
+        return require('../../trap-auth').signIn().then((user) => {
             return Promise.resolve((req, res, next) => {
                 req.brute.reset(function (err) {
                     if (err) {
