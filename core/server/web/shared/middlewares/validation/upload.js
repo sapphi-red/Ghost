e156078e85952bf757b11c15d9868dd046b6afa7
sapphi-red <green@sapphi.red>
2020-01-12 21:51:29 +0900

    画像以外のアップロードに対応

--- core/server/web/shared/middlewares/validation/upload.js
+++ core/server/web/shared/middlewares/validation/upload.js
@@ -16,6 +16,11 @@ module.exports = function upload(options) {
         req.file.name = req.file.originalname;
         req.file.type = req.file.mimetype;
 
+        // Skip validation
+        if (type === 'any') {
+            return next();
+        }
+
         // Check if a file was provided
         if (!localUtils.checkFileExists(req.file)) {
             return next(new errors.ValidationError({
