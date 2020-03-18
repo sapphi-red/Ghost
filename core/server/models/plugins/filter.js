3ea27ddfb8897536001b0657b8dbf9df5cfcc935
sapphi-red <green@sapphi.red>
2020-03-18 21:04:57 +0900

    一文字IDでフィルタがエラーを吐くのを修正

--- core/server/models/plugins/filter.js
+++ core/server/models/plugins/filter.js
@@ -76,6 +76,10 @@ const filter = function filter(Bookshelf) {
                 }
             }
 
+            if (custom) {
+                custom = custom.replace(/authors:(.)($|\+|\))/, 'authors:\'$1\'$2');
+            }
+
             try {
                 this.query((qb) => {
                     nql(custom, {
