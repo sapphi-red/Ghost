f3d860b827f2d8f187898c0f1b83e811f7dd2b28
sapphi-red <green@sapphi.red>
2020-04-04 16:34:09 +0900

    自分のメールアドレスは空文字にならないように
    bioが編集できなかったのを修正

--- core/server/models/user.js
+++ core/server/models/user.js
@@ -218,8 +218,6 @@ User = ghostBookshelf.Model.extend({
         var options = User.filterOptions(unfilteredOptions, 'toJSON'),
             attrs = ghostBookshelf.Model.prototype.toJSON.call(this, options);
 
-        attrs.email = '';
-
         // remove password hash for security reasons
         delete attrs.password;
 
